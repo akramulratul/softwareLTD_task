7047894e1692b893edc30c0bb02c5643cf72fad6
Akramul Ratul <akramulratul@gmail.com>
2023-09-03 20:38:43 +0600

    Environment setup in vite with tailwind css init

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  import TailwindCss from './lib/TailwindCSS.svelte';
+</script>
+<TailwindCss/>
+<main>
+  <p class="underline decoration-wavy ">The quick brown fox...</p>
+</main>
+
+<style>
+
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  content: ["./src/**/*.{html,js}"],
+  purge: ["./index.html", "./src/**/*.{svelte,js,ts}"],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+};
